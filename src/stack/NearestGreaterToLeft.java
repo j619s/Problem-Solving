a515b71aa48ffc22c90af17d196bca6879adb6ad
Jatin <jatinsingh619@gmail.com>
2024-03-28 23:45:07 +0530

    stack problems added.

--- src/stack/NearestGreaterToLeft.java
+++ src/stack/NearestGreaterToLeft.java
@@ -1,4 +1,40 @@
 package stack;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+
 public class NearestGreaterToLeft {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 2, 4};
+        System.out.println(ngl(arr));
+    }
+    /*
+    *  input: arr[] = [1, 3, 2, 4]
+    * output: arr[] = [-1, -1, 3, -1]
+    * */
+    public static List<Integer> ngl(int[] arr){
+        List<Integer> res = new ArrayList<>();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for(int i = 0;i < arr.length; i++){
+            if(stack.isEmpty()){
+                res.add(-1);
+            } else if(!stack.isEmpty() && stack.peek() > arr[i]){
+                res.add(stack.peek());
+            } else if(!stack.isEmpty() && stack.peek() < arr[i]){
+                while(!stack.isEmpty() && stack.peek()< arr[i]){
+                    stack.poll();
+                }
+                if(stack.isEmpty()){
+                    res.add(-1);
+                } else {
+                    res.add(stack.peek());
+                }
+            }
+            stack.push(arr[i]);
+        }
+
+        return res;
+    }
 }
--- src/stack/NearestGreaterToRight.java
+++ src/stack/NearestGreaterToRight.java
@@ -1,4 +1,45 @@
 package stack;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
 public class NearestGreaterToRight {
+    public static void main(String[] args) {
+            int[] arr = {1,3,2,4};
+            int n = arr.length;
+        System.out.println(ngr(arr, n));
+    }
+    /*
+    *  Problem Statement:
+    *   arr[] =  [1, 3, 2, 4]
+    *   output:  [3, 4, 4, -1]
+    *    find the greater element to the right of the current element
+    * */
+    public static List<Integer> ngr(int[] arr, int n){
+        Stack<Integer> stack = new Stack<>();
+        List<Integer> res = new ArrayList<>();
+
+        for(int i = n - 1; i>= 0; i--){
+            if(stack.isEmpty()){
+                res.add(-1);
+            } else if(!stack.isEmpty() && stack.peek() > arr[i]){
+                res.add(stack.peek());
+            } else if(!stack.isEmpty() && stack.peek() <= arr[i]){
+                while(!stack.isEmpty() && stack.peek() <= arr[i]){
+                    stack.pop();
+                }
+                if(stack.isEmpty()){
+                    res.add(-1);
+                } else {
+                    res.add(stack.peek());
+                }
+            }
+
+            stack.push(arr[i]);
+        }
+        Collections.reverse(res);
+        return res;
+    }
 }
